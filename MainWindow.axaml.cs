10649ceeb6e5ca3f7b9d4502eed58708b1b936b9
binbuf <261476663+binbuf@users.noreply.github.com>
2026-02-13 19:52:47 -0800

    initial import of fresh project

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Rawr
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
